003967b21b3753628da4b1707f506c75609b46fe
elvinibbotson <elvinibbotson@alittle.org.uk>
2018-12-11 20:55:18 +0000

    initialise

--- top-upScript.js
+++ top-upScript.js
@@ -350,8 +350,8 @@ function id(el) {
 	if (navigator.serviceWorker.controller) {
 		console.log('Active service worker found, no need to register')
 	} else { //Register the ServiceWorker
-		navigator.serviceWorker.register('sw.js', {
-			scope: '/Petrol/'
+		navigator.serviceWorker.register('top-upSW.js', {
+			scope: '/Top-up/'
 		}).then(function(reg) {
 			console.log('Service worker has been registered for scope:'+ reg.scope);
 		});
